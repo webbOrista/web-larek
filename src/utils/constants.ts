59882d449ded84b3c035c748d3f39192129c0a01
Boris Butorin <butorinb.g.personal@gmail.com>
2024-09-08 17:44:41 +0300

    Readme_updated

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,4 @@
+// Все могло бы выглядеть так, но gh-pages - статический хостинг
 // export const API_URL = `${process.env.API_ORIGIN}/api/weblarek`;
 // export const CDN_URL = `${process.env.API_ORIGIN}/content/weblarek`;
 
@@ -5,8 +6,3 @@ export const API_ORIGIN = 'https://larek-api.nomoreparties.co'
 export const API_URL = 'https://larek-api.nomoreparties.co/api/weblarek';
 export const CDN_URL = 'https://larek-api.nomoreparties.co/content/weblarek';
 export const API_TOKEN = '52e38bc9-9b95-4e67-8615-786acb426380'
-
-
-export const settings = {
-
-};
